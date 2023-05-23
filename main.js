226fdb6f03ce7bbb8c68bd0ad730ddb0429f5a0c
Soujanya-PLV <128033857+Soujanya-PLV@users.noreply.github.com>
2023-05-23 21:32:44 +0530

    main.js

--- main.js
+++ main.js
@@ -7,8 +7,9 @@
         "esri/widgets/Expand",
         "esri/widgets/Legend",
         "esri/widgets/Home",
-        //"esri/symbols/Point",
-        "esri/widgets/Search"
+        "esri/widgets/Search",
+        "esri/core/promiseUtils",
+        "esri/core/reactiveUtils"
          ], 
         function (
         WebMap,
@@ -19,60 +20,143 @@
         Expand,
         Legend,
         Home,
-        //PointSymbol3D,
-        Search
+        Search,
+        promiseUtils,
+        reactiveUtils
       )
          {
-         
+
+/*****************************************
+ * Define map and view
+*****************************************/
+   const map = new WebMap({
+      basemap: ({
+        baseLayers: [new TileLayer({
+          url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+          })]
+      }),
+      ground: {
+        surfaceColor: [255, 255, 255]
+      }
+    });
+
+    const view = new SceneView({
+      container: "viewDiv",   
+      camera: {
+      position: [78.9629, 20.5937, 20000000],
+      heading: 0, //face due east
+      tilt: 0 //looking from a bird's eye view
+       },     
+      qualityProfile: "high",
+      map: map,
+      alphaCompositingEnabled: true,
+      environment: {
+          background: {
+              type: "color",
+              color: "#f2f0e1"
+         },
+         starsEnabled: false,
+         atmosphereEnabled: false
+         },
+         highlightOptions: {
+         fillOpacity: 0,
+         },
+         });
+   
+/*********************************************
+  * Adding plateTectonic border to the map scene
+ ***********************************************/
+      
+ const plateTectonicBorders = new FeatureLayer({
+    url: "https://services2.arcgis.com/cFEFS0EWrhfDeVw9/arcgis/rest/services/plate_tectonics_boundaries/FeatureServer",
+    elevationInfo: {
+      mode: "on-the-ground"},
+     renderer: {
+      type: "simple",
+      symbol: {
+        type: "line-3d",
+        symbolLayers: [
+          {
+            type: "line",
+            material: { color: [224, 52, 40, 0.7] },
+            size: 3
+          }
+        ]
+      }
+    }
+  });
+  map.add(plateTectonicBorders);
+          
 /**********************************************
  Adding the PAGER-USGS data set to the mapscene
-***********************************************/         
+***********************************************/      
+
  const layer = new GeoJSONLayer ({
-          url: 
-   //"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson",
-   "https://earthquake.usgs.gov/fdsnws/event/1/query",
+          url: "https://earthquake.usgs.gov/fdsnws/event/1/query",
           copyright: "USGS-PAGER-Earthquakes",
-          // Use customParameters to set the query parameters
+          // Using customParameters to set the query parameters
           // get the all red alert earthquakes since 1905
           //order the results by magnitude
           customParameters: {
             format: "geojson",
+            starttime: "1905-01-01",
+            endtime: new Date().toISOString().split("T")[0],
             orderby: "magnitude",
             minmagnitude: 1,
-            eventtype: "earthquake",
-            // starttime: "1950-01-01",
-            // endtime: "2023-04-01",
-            alertlevel: "red",
-            // alertlevel: "green",
-            // alertlevel: "orange",
-            // alertlevel: "yellow"
-            producttype: "losspager"
-          },
+            alertlevel: "red"        
+           },
    
-          // // only show earthquakes that mentions japan
-          definitionExpression: "",
-          //effect: "bloom(2 1px 0)",
-          title: "USGS Earthquakes",
+          // to show earthquakes around the world
+          definitionExpression: "place LIKE '%'",
+            title: "USGS Earthquakes",
             renderer: {
-            // apply unique values to alert levels
+            // apply unique values to alert levels and adding their visualisation parameters
               type: "unique-value",
               field: "alert",
                 uniqueValueInfos: [
                   {
                     value: "red",
-                    symbol: createQuakeSymbol("red")
-                   },
+                    symbol: {
+              type: "simple-marker",
+              style: "circle",
+              outline: {
+                  color: "red",
+                  width: 2
+                       }
+                     }
+                  },
                   {
                      value: "orange",
-                     symbol: createQuakeSymbol("orange")
+                     symbol:  {
+              type: "simple-marker",
+              style: "circle",
+              outline: {
+                  color: "orange",
+                  width: 2
+                       }
+                     }
                   },
                   {
                       value: "yellow",
-                      symbol: createQuakeSymbol("yellow")
+                      symbol:  {
+              type: "simple-marker",
+              style: "circle",
+              outline: {
+                  color: "yellow",
+                  width: 2
+                       }
+                     }
                   },
                   {
                       value: "green",
-                      symbol: createQuakeSymbol("#136d15")
+                      symbol:  {
+              type: "simple-marker",
+              style: "circle",
+              outline: {
+                  color: "#136d15",
+                  width: 2
+                       }
+                     }
                   }
                 ],
           visualVariables: [
@@ -96,8 +180,8 @@
               }
             ]
           },
-         
-   // Adding a popup template to each earthquake
+   
+// Adding a popup template to each earthquake
    popupTemplate: {
             title: "Earthquake Info",
             content:
@@ -158,68 +242,8 @@
             "resultsHeading"
           ).innerHTML = `<b>${features.length}</b> ${layer.customParameters.alertlevel} alert level earthquakes.`;
         }
-
-/*****************************************
- * Define map and view
-*****************************************/
-   const map = new WebMap({
-      basemap: ({
-        baseLayers: [new TileLayer({
-          url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
-          })]
-      }),
-      ground: {
-        surfaceColor: [255, 255, 255]
-      }
-    });
-
-    const view = new SceneView({
-      container: "viewDiv",   
-      camera: {
-      position: [78.9629, 20.5937, 20000000],
-      heading: 0, //face due east
-      tilt: 0 //looking from a bird's eye view
-       },     
-      qualityProfile: "high",
-      map: map,
-      alphaCompositingEnabled: true,
-      environment: {
-          background: {
-              type: "color",
-              color: "#f2f0e1"
-         },
-         starsEnabled: false,
-         atmosphereEnabled: false
-         },
-         highlightOptions: {
-         fillOpacity: 0,
-         },
-         });
    
-   /*********************************************
-  * Adding plateTectonic border to the map scene
- ***********************************************/
-      
- const plateTectonicBorders = new FeatureLayer({
-    url: "https://services2.arcgis.com/cFEFS0EWrhfDeVw9/arcgis/rest/services/plate_tectonics_boundaries/FeatureServer",
-    elevationInfo: {
-      mode: "on-the-ground"},
-     renderer: {
-      type: "simple",
-      symbol: {
-        type: "line-3d",
-        symbolLayers: [
-          {
-            type: "line",
-            material: { color: [224, 52, 40, 0.7] },
-            size: 3
-          }
-        ]
-      }
-    }
-  });
-  map.add(plateTectonicBorders);
-      
+map.add(layer);
 /*********************************************
 * Adding legend to the map scene
 ***********************************************/
@@ -237,7 +261,7 @@
      
  /*********************************************
  * Adding Home button to the map scene
- ***********************************************/
+***********************************************/
  
 // Add the home button to the top left corner of the view
 		const homeBtn = new Home({
@@ -247,7 +271,7 @@
 
  /*********************************************
  * Adding Search button to the map scene
- ***********************************************/
+***********************************************/
  // A. specify content with a widget
    let searchWidget = new Search({
      view: view
@@ -259,17 +283,4 @@
      content: searchWidget
    });
    view.ui.add(expand, "top-left");
-
-// assign symbols to earthquakes matching their alert level.
-   
-        function createQuakeSymbol(color) {
-          return {
-            type: "point-3d",
-            //color: null,
-            outline: {
-              color: color,
-              width: "2px"
-            }
-          };
-        };
-   });
+    });
